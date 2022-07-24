7febeaaa2cc5c5acd5c45b536d19f0b8ec9dffd5
Pontes Neto Dev <pontesneto2@gmail.com>
2022-07-24 18:13:00 -0300

    up index.php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora IMC</title>
-</head>
-<body>
     <div>
         <p><b>O seu IMC é aproximadamente:</b></p>
             <?php
@@ -30,6 +21,3 @@
                 echo "E seu peso tem o padrão: ", $faixa,".";
             ?>
     </div>
-
-</body>
-</html>
